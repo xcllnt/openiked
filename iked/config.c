2dfb86792e798c56158ebfdab72b5b82aa027485
Marcel Moolenaar <marcel@brkt.com>
2016-10-25 21:39:45 -0700

    Add my copyright

--- iked/config.c
+++ iked/config.c
@@ -2,6 +2,7 @@
 
 /*
  * Copyright (c) 2010-2013 Reyk Floeter <reyk@openbsd.org>
+ * Copyright (c) 2016 Marcel Moolenaar <marcel@brkt.com>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
